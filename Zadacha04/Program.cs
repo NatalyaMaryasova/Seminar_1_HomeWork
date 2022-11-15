5bd90097e5308b6efcab0087877fc86b4edebbae
Natalya <nanona@example.com>
2022-11-15 21:21:04 +0700

    Added zadacha04

---
+++ Zadacha04/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num0 = 2;
+
+while(num0 <= num)
+{
+    Console.Write(num0 + " ");
+    num0 = num0 + 2;
+}
