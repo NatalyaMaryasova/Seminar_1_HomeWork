840cacc0a4716b2ae212cd44617e7b084753bc2a
Natalya <nanona@example.com>
2022-11-15 21:19:59 +0700

    Added zadacha01

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго");
+    int max = num1;
+    Console.WriteLine("max" + "=" + num1);
+}
+if(num1 < num2)
+{
+    Console.WriteLine("Второе число больше первого");
+    int max = num2;
+    Console.WriteLine("max" + "=" + num2);
+}
+if(num1 == num2)
+{
+    Console.WriteLine("Числа равны");
+}
